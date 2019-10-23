a34033185a28216effebc7fbb772027d66e81696
xearts <xearts@mbp-13-early2011-01.local>
2019-10-23 16:15:27 +0900

    素数判定プログラムを作成

---
+++ func.php
@@ -0,0 +1,28 @@
+<?php
+
+// 素数判定関数(true:素数,false:素数ではない)
+function isPrimeNumber($num):bool
+{
+
+    if ($num <= 1) {
+        //1以下は素数ではない
+        return false;
+
+    } elseif ($num === 2) {
+        //1の時は素数である
+        return true;
+
+    } else {
+        //約数があるかを確認
+        for ($i = 2; $i < $num; $i++) {
+
+            if ($num % $i === 0) {
+                return false;
+            }
+
+        }
+
+    }
+    return true;
+
+}
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once 'func.php';
+$state = false;
+if (isset($_GET['number'])) {
+    $state = true;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>素数判定</title>
+</head>
+
+<boby>
+    <h1>素数判定</h1>
+    <p>入力した数字が素数か素数でないかを判定します。</p>
+
+    <form action="#" method="get">
+        <input type="number" name="number">
+        <input type="submit">
+    </form>
+
+    <?php if ($state): //入力状態がtrueのとき ?>
+        <?php $number = (int)$_GET['number']; //入力された値を代入 ?>
+
+        <h3>判定結果：
+
+            <?php if (isPrimeNumber($number)): ?>
+                <?php echo '素数である'; ?>
+            <?php elseif (!isPrimeNumber($number)): ?>
+                <?php echo '素数でない'; ?>
+            <?php endif; ?>
+
+        </h3>
+
+    <?php endif; ?>
+
+</boby>
+</html>
