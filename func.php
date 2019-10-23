7a208e325c998f63bf117638c0cbc6a65fbcfdbd
xearts <xearts@mbp-13-early2011-01.local>
2019-10-23 18:31:25 +0900

    コマンド起動できるようにした。処理速度の向上

--- func.php
+++ func.php
@@ -3,27 +3,19 @@
 /**
  * 素数判定関数(true:素数,false:素数ではない)
  *
- * @param int $num
+ * @param float $num
  * @return bool
  */
 function isPrimeNumber(int $num): bool
 {
-    if ($num <= 1) {
-        //1以下は素数ではない
+    if ($num === 1) {
+        //1は素数ではない
         return false;
-
-    } elseif ($num === 2) {
-        //1の時は素数である
-        return true;
-
     } else {
-        //約数があるかを確認
-        for ($i = 2; $i < $num; $i++) {
-
+        for ($i = 2; $i <= (int)sqrt($num); ++$i) {
             if ($num % $i === 0) {
                 return false;
             }
-
         }
     }
     return true;
--- index.php
+++ index.php
@@ -1,44 +1,12 @@
 <?php
 
 require_once 'func.php';
-$state = false;
-if (isset($_GET['number'])) {
-    $state = true;
+
+echo '数字を入力してください',PHP_EOL;
+$number = (int)trim(fgets(STDIN));
+
+if (isPrimeNumber($number)) {
+    echo '素数である';
+} else {
+    echo '素数ではない';
 }
-
-?>
-
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>素数判定</title>
-</head>
-
-<boby>
-    <h1>素数判定</h1>
-    <p>入力した数字が素数か素数でないかを判定します。</p>
-
-    <form action="#" method="get">
-        <input type="number" name="number">
-        <input type="submit">
-    </form>
-
-    <?php if ($state): //入力状態がtrueのとき ?>
-        <?php $number = (int)$_GET['number']; //入力された値を代入 ?>
-
-        <h3>判定結果：
-
-            <?php if (isPrimeNumber($number)): ?>
-                <?php echo '素数である'; ?>
-            <?php else: ?>
-                <?php echo '素数でない'; ?>
-            <?php endif; ?>
-
-        </h3>
-
-    <?php endif; ?>
-
-</boby>
-</html>
