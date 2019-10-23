dcb15fdfd70603ab795ab0eec4b2e5cf8714465f
xearts <xearts@mbp-13-early2011-01.local>
2019-10-23 19:15:45 +0900

    isPrimeNumber関数の修正

--- func.php
+++ func.php
@@ -8,16 +8,16 @@
  */
 function isPrimeNumber(int $num): bool
 {
-    if ($num === 1) {
-        //1は素数ではない
+    if ($num <= 1) {
         return false;
-    } else {
-        $root = (int)sqrt($num);
-        for ($i = 2; $i <= $root; ++$i) {
-            if ($num % $i === 0) {
-                return false;
-            }
+    }
+
+    $max = (int)sqrt($num);
+    for ($i = 2; $i <= $max; ++$i) {
+        if ($num % $i === 0) {
+            return false;
         }
     }
+
     return true;
 }
